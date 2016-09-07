570a52e05ed37038e294e71821778bc3f23198b6
Frederic Jacob <frederic.jacob.78@gmail.com>
2016-09-06 21:45:00 -0400

    Add Source Highlight module

--- modules/ex1.js
+++ modules/ex1.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './navbar'
+var Highlight = require('react-highlight');
 
 class Child extends React.Component{
         constructor(props) {
@@ -24,6 +25,41 @@ class ParentEx1 extends React.Component{
                 return (
                         <div className='parent'>
                                 <Child> Parent </Child>
+                                        <Highlight className='hljs'>
+          {`import React from 'react'
+          import Navbar from './navbar'
+          var Highlight = require('react-highlight');
+
+          class Child extends React.Component{
+                  constructor(props) {
+                          super(props);
+                  }
+
+                  render(){
+                          return (
+                                  <div className='child'>
+                                          Super Child 👶👶👶
+                                          {this.props.children}
+                                  </div>)
+                  }
+          }
+
+          class ParentEx1 extends React.Component{
+                  constructor(props) {
+                          super(props);
+                  }
+
+                  render(){
+                          return (
+                                  <div className='parent'>
+                                          <Child> Parent </Child>
+                                          <Navbar prev='' next='ex2'></Navbar>
+                                  </div>)
+                  }
+          }
+
+          module.exports = ParentEx1;`}
+        </Highlight>
                                 <Navbar prev='' next='ex2'></Navbar>
                         </div>)
         }
